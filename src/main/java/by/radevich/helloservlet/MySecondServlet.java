4f6a03040cb812602b3cfb3cfc07aadae366d06a
Notebook <magog21@gmail.com>
2023-06-12 19:18:12 +0300

    ClassWorkLessons21

--- src/main/java/by/radevich/helloservlet/MySecondServlet.java
+++ src/main/java/by/radevich/helloservlet/MySecondServlet.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.stream.Collectors;
@@ -24,7 +25,8 @@ public class MySecondServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("servlet path: " + req.getServletPath());//путь
         System.out.println("servlet URL: " + req.getRequestURI());//путь полный
-        System.out.println("Parametr: " + req.getParameter("my"));
+        System.out.println("Parametr: " + req.getParameter("my"));//для дз
+        System.out.println("date: "+ LocalDate.now().getYear());
         //ищем парпметр в ссылке пишем вопрос потом abc?my123&another=empty
         HttpSession session = req.getSession();//создаем сессию
         Integer currentParam = (Integer) session.getAttribute("count");
